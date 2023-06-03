84cd6b33e71c7aa6e90e9d225c78a1a726718e75
Augustin Ayama <122065729+MarketingIQ@users.noreply.github.com>
2023-06-03 21:02:58 +0100

    Initialize MarketingIQ-patch-1

---
+++ FBSDKCoreKit/FBSDKCoreKitTests/Internal/Helpers/TestCrashHandler.swift
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import FBSDKCoreKit
+import XCTest
+
+@objcMembers
+final class TestCrashHandler: NSObject, CrashHandlerProtocol {
+
+  var wasAddObserverCalled = false
+  var observer: CrashObserving?
+  var wasClearCrashReportFilesCalled = false
+  var wasSaveExceptionCalled = false
+
+  func addObserver(_ observer: CrashObserving) {
+    wasAddObserverCalled = true
+    self.observer = observer
+  }
+
+  func clearCrashReportFiles() {
+    wasClearCrashReportFilesCalled = true
+  }
+
+  func save(_ exception: NSException) {
+    wasSaveExceptionCalled = true
+  }
+}
